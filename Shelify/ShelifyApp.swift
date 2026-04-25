f2f23f0ce4db5b4f4fc090bc92bc6fb59ad6c73f
Hannah Dadd <hannahbdadd@googlemail.com>
2026-04-25 08:03:40 +0100

    Initial Commit

---
+++ Shelify/ShelifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShelifyApp.swift
+//  Shelify
+//
+//  Created by Hannah Dadd on 25/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShelifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
