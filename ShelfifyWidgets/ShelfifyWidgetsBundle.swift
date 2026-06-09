615c3e3fb4916fc784073a9e6b68ab001245582a
Hannah Dadd <hannahbdadd@googlemail.com>
2026-06-09 08:27:25 +0100

    widget

---
+++ ShelfifyWidgets/ShelfifyWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ShelfifyWidgetsBundle.swift
+//  ShelfifyWidgets
+//
+//  Created by Hannah Dadd on 08/06/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ShelfifyWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        ShelfifyWidgets()
+    }
+}
