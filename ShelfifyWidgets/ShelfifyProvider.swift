2b67d6f6316567c42f43853244f905b0e0683840
Hannah Dadd <hannahbdadd@googlemail.com>
2026-06-09 18:37:11 +0100

    widgets provider

---
+++ ShelfifyWidgets/ShelfifyProvider.swift
@@ -0,0 +1,35 @@
+//
+//  ShelfifyProvider.swift
+//  Shelfify
+//
+//  Created by Hannah Dadd on 09/06/2026.
+//
+
+import WidgetKit
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), assettName: "6")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), assettName: "6")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+        let widgetAssetNames = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, assettName: widgetAssetNames.randomElement() ?? "8")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
--- ShelfifyWidgets/ShelfifyWidgets.swift
+++ ShelfifyWidgets/ShelfifyWidgets.swift
@@ -8,52 +8,16 @@
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-
-//    func relevances() async -> WidgetRelevances<Void> {
-//        // Generate a list containing the contexts this widget is relevant in.
-//    }
-}
-
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let emoji: String
+    let assettName: String
 }
 
 struct ShelfifyWidgetsEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
-        }
+        Image(entry.assettName)
     }
 }
 
@@ -71,14 +35,10 @@ struct ShelfifyWidgets: Widget {
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Shelfify Widget")
+        .supportedFamilies([
+            .systemMedium
+        ])
+        .description("Widgets to keep you writing.")
     }
-}
-
-#Preview(as: .systemSmall) {
-    ShelfifyWidgets()
-} timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
 }
